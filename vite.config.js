7e9eab722f0e18ec2818dee283f380651903bc38
Ayush7621 <ayushpatil2167@gmail.com>
2025-05-14 00:27:53 +0530

    Initial commit14

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  //base: "/ayush-portfolio",
+  
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
